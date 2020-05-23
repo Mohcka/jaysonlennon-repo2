0891a8211e45023d102646200894a9c018dd8591
Jayson Lennon <jayson@jaysonlennon.dev>
2020-05-23 10:45:31 -0700

    Add dummy test for coverage check

--- Project2/Test/TestApi/UnitTest1.cs
+++ Project2/Test/TestApi/UnitTest1.cs
@@ -1,14 +1,31 @@
+using System.Reflection;
 using System;
 using Xunit;
+
+using AptMgmtPortal;
 
 namespace TestApi
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FailingTest()
         {
+            Assert.True(false);
+        }
 
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Coverage()
+        {
+            var someClass = new WeatherForecast();
+            someClass.Summary = "nothing";
+            Assert.True(true);
         }
     }
 }
