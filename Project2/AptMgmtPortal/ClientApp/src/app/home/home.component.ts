4ccd91ffd2606f26669d894e9fb1444271b521c5
Mohcka <mikeduels@yahoo.com>
2020-05-27 18:01:20 -0700

    Adds users to home page

--- Project2/AptMgmtPortal/ClientApp/src/app/home/home.component.ts
+++ Project2/AptMgmtPortal/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public users: User[] = [];
+  constructor(private userService: UserService) {}
+
+  public ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe((users) => (this.users = users));
+  }
 }
