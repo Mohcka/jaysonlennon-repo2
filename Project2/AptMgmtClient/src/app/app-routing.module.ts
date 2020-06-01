58d52d4a3b31d935511a150bc576dff3be9b5f67
David Sawyer <desawyer85@gmail.com>
2020-06-01 13:00:36 -0500

    updates routing

--- Project2/AptMgmtClient/src/app/app-routing.module.ts
+++ Project2/AptMgmtClient/src/app/app-routing.module.ts
@@ -8,6 +8,12 @@ import { TenantDetailsComponent } from './components/tenant.components/tenant-de
 import { TenantsComponent } from './components/tenant.components/tenants/tenants.component';
 import { AuthGuard } from './guard/auth.guard';
 import { LoginComponent } from './components/login/login.component';
+import { LeaseTenComponent } from './components/tenant/lease-ten/lease-ten.component';
+import { MaintenanceTenComponent } from './components/tenant/maintenance-ten/maintenance-ten.component';
+import { PaymentTenComponent } from './components/tenant/payment-ten/payment-ten.component';
+import { LeaseManComponent } from './components/manager/lease-man/lease-man.component';
+import { MaintenanceManComponent } from './components/manager/maintenance-man/maintenance-man.component';
+import { PaymentManComponent } from './components/manager/payment-man/payment-man.component';
 
 // Define routes for the application
 const routes: Routes = [
@@ -21,6 +27,15 @@ const routes: Routes = [
   { path: 'tenant-detail/:id', component: TenantDetailsComponent },
   // * Maintenance
   { path: 'maintenance-requests', component: MaintenanceRequestsComponent, canActivate: [AuthGuard] },
+  { path: 'manager/maintenance', component: MaintenanceManComponent },
+  { path: 'tenant/maintenance', component: MaintenanceTenComponent },
+  
+  // * Payments Pages
+  { path: 'manager/payment', component: PaymentManComponent },
+  { path: 'tenant/payment', component: PaymentTenComponent },
+  // * Lease Pages
+  { path: 'manager/lease', component: LeaseManComponent },
+  { path: 'tenant/lease', component: LeaseTenComponent },
 ];
 
 @NgModule({
