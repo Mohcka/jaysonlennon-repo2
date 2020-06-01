efa9eac1ae054ab4363aa4766a850ed483c8e052
Jayson Lennon <jayson@jaysonlennon.dev>
2020-06-01 13:53:36 -0700

    Add route to get user by ID

--- Project2/AptMgmtPortalAPI/Controllers/AgreementController.cs
+++ Project2/AptMgmtPortalAPI/Controllers/AgreementController.cs
@@ -10,57 +10,40 @@ using AptMgmtPortalAPI.Types;
 namespace AptMgmtPortalAPI.Controllers.Tenant
 {
     [ApiController]
-    [Route("api/v1/User")]
-    public class UserController : ControllerBase
+    [Route("api/v1")]
+    public class AgreementController : ControllerBase
     {
-        private readonly ILogger<UserController> _logger;
+        private readonly ILogger<AgreementController> _logger;
         private readonly Repository.IUser _userRepository;
-        public UserController(ILogger<UserController> logger, Repository.IUser userRepository)
+        private readonly Repository.ITenant _tenantRepository;
+        public AgreementController(ILogger<AgreementController> logger,
+                                   Repository.IUser userRepository,
+                                   Repository.ITenant tenantRepository)
         {
             this._logger = logger;
             this._userRepository = userRepository;
+            this._tenantRepository = tenantRepository;
         }
 
         [HttpGet]
+        [Route("Agreements")]
         [Authorize(Policy = Policies.AnyLoggedIn)]
-        public async Task<IActionResult> GetUserInfo()
+        public async Task<IActionResult> GetAgreements()
         {
             var userId = this.UserIdFromApiKey();
             var user = await _userRepository.UserFromId(userId);
+            if (user == null)
+            {
+                var err = new DTO.ErrorBuilder()
+                                 .Message("Not a user")
+                                 .Code(400)
+                                 .Build();
+                return err;
+            }
 
             var userDTO = new DTO.UserDTO(user);
 
             return new ObjectResult(userDTO);
-        }
-
-        [HttpGet]
-        [Route("{userId}")]
-        [Authorize(Policy = Policies.AnyLoggedIn)]
-        public async Task<IActionResult> GetSpecificUserInfo(int userId)
-        {
-            if (this.HttpContext.User.IsInRole(Role.Admin) || this.HttpContext.User.IsInRole(Role.Manager)) {
-                var thisUserId = this.UserIdFromApiKey();
-                
-                var user = await _userRepository.UserFromId(userId);
-                if (user == null)
-                {
-                    var err = new DTO.ErrorBuilder()
-                                    .Message("No user found with that ID")
-                                    .Code(400)
-                                    .Build();
-                    return err;
-                }
-
-                var userDTO = new DTO.UserDTO(user);
-                return new ObjectResult(userDTO);
-
-            } else {
-                var err = new DTO.ErrorBuilder()
-                                .Message("You do not have the proper authorization to view user accounts.")
-                                .Code(403)
-                                .Build();
-                return err;
-            }
         }
 
         [HttpPost]
