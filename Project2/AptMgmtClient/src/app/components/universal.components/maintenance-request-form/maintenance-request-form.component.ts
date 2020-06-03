6a757731f55204858b5bc5fee3a81ec741e32d6d
Mohcka <mikeduels@yahoo.com>
2020-06-03 11:17:02 -0700

    Adds manager-maintenace functionality

--- Project2/AptMgmtClient/src/app/components/universal.components/maintenance-request-form/maintenance-request-form.component.ts
+++ Project2/AptMgmtClient/src/app/components/universal.components/maintenance-request-form/maintenance-request-form.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MaintenanceService } from 'src/app/services/maintenance.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { TenantService } from 'src/app/services/tenant.service';
+import { Tenant } from 'src/app/model/tenant';
+import { ResourcesStrArr } from 'src/enums/Resource';
+import { MaintenanceRequestData } from 'src/app/model/maintenance-request-data';
 
 @Component({
   selector: 'app-maintenance-request-form',
@@ -21,29 +26,58 @@ export class MaintenanceRequestFormComponent implements OnInit {
   public returnUrl: string;
   public error = '';
 
+  public tenants: Tenant[];
+
   // TODO: Get maintenance request type samples from server
-  public maintenanceRequestTypes: string[] = [
-    'Plumbing',
-    'Upholstry',
-    'Electrical',
-  ];
+  public maintenanceRequestTypes: string[] = ResourcesStrArr;
 
   constructor(
     private maintenanceService: MaintenanceService,
+    private tenantService: TenantService,
+    private authService: AuthenticationService,
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.maintenanceRequestForm = this.formBuilder.group({
-      type: ['Electrical', Validators.required],
+    this.getTenants();
+
+    let formGroupFields: any = {
+      type: [ResourcesStrArr[0], Validators.required],
       notes: ['', Validators.required],
-    });
+    };
+
+    if (this.isManager) {
+      formGroupFields = {
+        ...formGroupFields,
+        tenantUnit: ['', Validators.required],
+      };
+    }
+
+    this.maintenanceRequestForm = this.formBuilder.group(formGroupFields);
   }
 
   get f() {
     return this.maintenanceRequestForm.controls;
+  }
+
+  get isManager() {
+    return this.authService.currentUserIsManager();
+  }
+
+  getTenants(): void {
+    if (!this.isManager) {
+      return;
+    }
+
+    this.tenantService.getTenants().subscribe((tenants) => {
+      this.tenants = tenants;
+
+      this.maintenanceRequestForm.patchValue({
+        tenantUnit: tenants[0].unitNumber,
+      });
+    });
   }
 
   onSubmit(): void {
@@ -51,6 +85,9 @@ export class MaintenanceRequestFormComponent implements OnInit {
     console.log('maintenance request submitted');
     console.log(this.f.type.value);
     console.log(this.f.notes.value);
+    console.log(this.f.tenantUnit.value);
+
+    // return;
 
     this.submitted = true;
 
@@ -59,11 +96,24 @@ export class MaintenanceRequestFormComponent implements OnInit {
       return;
     }
 
+    let maintenanceRequestData: MaintenanceRequestData = {
+      openNotes: this.f.notes.value,
+      maintenanceRequestType: this.f.type.value,
+    };
+
+    if (this.isManager) {
+      maintenanceRequestData = {
+        ...maintenanceRequestData,
+        unitNumber: this.f.tenantUnit.value,
+      };
+    }
+
     this.maintenanceService
-      .createNewRequest({
-        openNotes: this.f.notes.value,
-        maintenanceRequestType: this.f.type.value,
-      })
-      .subscribe((_) => this.router.navigate(['/tenant/maintenance/list']));
+      .createNewRequest(maintenanceRequestData)
+      .subscribe((_) =>
+        this.router.navigate([
+          `/${this.isManager ? 'manager' : 'tenant'}/maintenance/list`,
+        ])
+      );
   }
 }
--- Project2/AptMgmtClient/src/app/components/universal.components/maintenance-requests/maintenance-requests.component.ts
+++ Project2/AptMgmtClient/src/app/components/universal.components/maintenance-requests/maintenance-requests.component.ts
@@ -2,21 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { MaintenanceService } from 'src/app/services/maintenance.service';
 import { MaintenanceRequest } from 'src/app/model/maintenance-request';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { MaintenanceRequestData } from 'src/app/model/maintenance-request-data';
+import { MaintenanceRequestUpdate } from 'src/app/model/maintenance-update';
 
 @Component({
   selector: 'app-maintenance-requests',
   templateUrl: './maintenance-requests.component.html',
-  styleUrls: ['./maintenance-requests.component.css']
+  styleUrls: ['./maintenance-requests.component.css'],
 })
 export class MaintenanceRequestsComponent implements OnInit {
   public maintenanceRequests: MaintenanceRequest[];
-  constructor(private maintenanceService: MaintenanceService, public authService: AuthenticationService) { }
+  constructor(
+    private maintenanceService: MaintenanceService,
+    public authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
     this.getAllRequests();
   }
 
+  get isManager(): boolean {
+    return this.authService.currentUserIsManager();
+  }
+
+  get isTenant(): boolean {
+    return this.authService.currentUserIsTenant();
+  }
+
+  cancelRequest(request: MaintenanceRequestUpdate): void {
+    this.maintenanceService
+      .cancelRequest({ ...request, closed: true })
+      .subscribe((_) => this.getAllRequests());
+  }
+
   public getAllRequests(): void {
-    this.maintenanceService.getAll().subscribe(requests => this.maintenanceRequests = requests);
+    this.maintenanceService.getAll().subscribe(
+      (requests) =>
+        (this.maintenanceRequests = requests.sort((a, b) => {
+          // Sort list by id
+          if (a.maintenanceRequestId < b.maintenanceRequestId) {
+            return -1;
+          }
+          if (b.maintenanceRequestId < a.maintenanceRequestId) {
+            return 1;
+          }
+          return 0;
+        }))
+    );
   }
 }
