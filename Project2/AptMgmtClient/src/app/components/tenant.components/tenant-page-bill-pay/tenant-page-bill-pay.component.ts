dc5465dd3726c2ac1672a904ea177c0f45df1334
Jayson Lennon <jayson@jaysonlennon.dev>
2020-06-04 12:58:06 -0700

    Add billing details to bill page

--- Project2/AptMgmtClient/src/app/components/tenant.components/tenant-page-bill-pay/tenant-page-bill-pay.component.ts
+++ Project2/AptMgmtClient/src/app/components/tenant.components/tenant-page-bill-pay/tenant-page-bill-pay.component.ts
@@ -1,3 +1,4 @@
+import { ResourceUsageService } from './../../../services/resource-usage.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { BillService } from 'src/app/services/bill.service';
 import { Component, OnInit } from '@angular/core';
@@ -5,6 +6,8 @@ import { Bill } from 'src/app/model/bill';
 import { Resource } from 'src/enums/Resource';
 import { TenantService } from 'src/app/services/tenant.service';
 import { Tenant } from 'src/app/model/tenant';
+import { ResourceUsageProjection } from 'src/app/model/resource-usage-projection';
+import { MeteredResouceUsageEntry } from 'src/app/model/metered-resource-usage-entry';
 
 @Component({
   selector: 'app-tenant-page-bill-pay',
@@ -15,15 +18,39 @@ export class TenantPageBillPayComponent implements OnInit {
 
   public bill: Bill;
   public tenant: Tenant;
+  
+  usageSummary: ResourceUsageProjection;
+  usageData: MeteredResouceUsageEntry[];
+  unitName: string;
 
   constructor(private billService: BillService,
               private tenantService: TenantService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private resourceUsageService: ResourceUsageService) { }
 
   ngOnInit(): void {
     this.getBill();
     this.getTenant();
   }
+
+  getUsageData() {
+    this.resourceUsageService.getUsageEntriesForResource(this.bill.resource)
+    .subscribe(data => this.usageData = data);
+  }
+
+  getSummaryData() {
+    this.resourceUsageService.getProjectionForResource(this.bill.resource)
+      .subscribe(summary => this.usageSummary = summary);
+  }
+
+  configureUnitName() {
+    switch (this.bill.resource) {
+      case Resource.Power: this.unitName = 'kWh'; break;
+      case Resource.Water: this.unitName = 'cu.ft.'; break;
+      default: this.unitName = '';
+    }
+  }
+
 
   getBill(): void {
     const billingPeriod = this.activatedRoute.snapshot.paramMap.get('periodId');
@@ -32,6 +59,9 @@ export class TenantPageBillPayComponent implements OnInit {
       .getBillsInPeriod(Number(billingPeriod))
       .subscribe(bills => {
         this.bill = bills.filter(b => b.resource === Number(resourceTypeId)).pop();
+        this.configureUnitName();
+        this.getSummaryData();
+        this.getUsageData();
       });
 
   }
